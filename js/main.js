bf00cfd2021646c642a9379c3d930c8f20d02111
Amr Mohamed <63131532+amr-mohamed1@users.noreply.github.com>
2020-07-02 14:44:16 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$("document").ready(function () {
+    "use strict";
+    
+    // nice scroll plugin
+        $("body").niceScroll({
+        cursorcolor: "#f9218d",
+        cursorwidth: "8px",
+        cursorborder: "1px solid #f9218d"
+    });
+    
+     $(".header").height($(window).height());
+
+    $(window).resize(function () {
+        $(".header").height($(window).height());
+    });
+    $(".header .header-info").each(function () {
+        $(this).css('paddingTop', ($(window).height() - $(".navbar").height()) / 2 - 140);
+    });
+    
+    // auto write
+        var typed = new Typed('.writter', {
+        strings: ["<p>attractive visual learning experience</p>", "<p>Get daily tasks and bounes</p>"],
+        typeSpeed: 50,
+        backSpeed: 20,
+        backDelay: 1000,
+        loop: true
+    });
+});
